5a4cf74015697ed42128f217c6ede3c96b48b040
Arpit <menariaarpit2@gmail.com>
2025-04-27 23:07:22 +0530

    Updated frontend/backend code

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs'); // Import fs module
 const fileRoutes = require('./routes/fileRoutes');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+// Create the uploads folder if it doesn't exist
+const uploadsDir = './uploads';
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
 
 app.use(cors());
 app.use(express.json());
